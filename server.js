e5c1bcfcb0a13e95572fbe6875f4427bb73b04e3
ericDK89 <ericthr42@gmail.com>
2024-07-25 05:32:08 -0300

    cors

--- server.js
+++ server.js
@@ -3,25 +3,27 @@ const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
-// Use the default middleware
-const middlewares = jsonServer.defaults();
-
 // Configure CORS options
 const corsOptions = {
   origin: '*', // Permite todas as origens. Altere conforme necessário.
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   preflightContinue: false,
   optionsSuccessStatus: 204
 };
 
+// Use the CORS middleware
 server.use(cors(corsOptions));
+
+// Use the default middleware
+const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 // Add this before server.use(router)
 server.use(
   // Add custom route here if needed
   jsonServer.rewriter({
-    "/*": "/$1",
+    "/api/*": "/$1",
   })
 );
 
@@ -29,7 +31,7 @@ server.use(router);
 
 // Listen to port
 server.listen(3000, () => {
-  console.log("JSON Server is running");
+  console.log("JSON Server is running on port 3000");
 });
 
 // Export the Server API
